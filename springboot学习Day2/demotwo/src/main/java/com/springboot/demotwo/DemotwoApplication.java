72c1430be1a119d020ebc746c903f79647146dca
“cungudafa” <cungudafa@qq.com>
2019-08-09 14:31:04 +0800

    springbootdaytwo

---
+++ springboot学习Day2/demotwo/src/main/java/com/springboot/demotwo/DemotwoApplication.java
@@ -0,0 +1,13 @@
+package com.springboot.demotwo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemotwoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DemotwoApplication.class, args);
+    }
+
+}
---
+++ springboot学习Day2/demotwo/src/main/java/com/springboot/demotwo/jpa/UserJPA.java
@@ -0,0 +1,24 @@
+package com.springboot.demotwo.jpa;
+
+import com.springboot.demotwo.entity.UserEntity;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
+
+
+import javax.transaction.Transactional;
+import java.io.Serializable;
+
+/**
+ * 数据库操作
+ * SpringBoot+SpringDataJPA
+ * SpringDataJPA内部使用了类代理的方式
+ * 让继承了它接口的子接口都以spring管理的Bean的形式存在，
+ * 也就是说我们可以直接使用@Autowired注解在spring管理bean使用
+ */
+public interface UserJPA extends
+        JpaRepository<UserEntity,Long>,//SpringDataJPA提供的简单数据操作接口
+        JpaSpecificationExecutor<UserEntity>, //SpringDataJPA提供的复杂查询接口
+        Serializable {
+}
